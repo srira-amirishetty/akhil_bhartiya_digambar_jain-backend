6efc8a790ea560997908b8f2c6e664c08b8756ff
ishit10 <kapuparaishit18@gmail.com>
2025-05-07 11:57:16 +0530

    6,32,33are done

--- page32 and 33/index.js
+++ page32 and 33/index.js
@@ -10,10 +10,15 @@ connectDB();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
+app.use('/api/form', require('./routes/shayataAppform'));
 app.use('/api/member-update', memberUpdateRoutes);
 app.use('/api/member-list', memberListRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Shayata API');
+  });
+  
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
